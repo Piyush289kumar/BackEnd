bce222756f2bca85bb406790beec917dd51ff719
Piyush289kumar <piyushraikwar289@gmail.com>
2024-04-08 11:18:54 +0530

    Error on .env file;

--- src/db/index.js
+++ src/db/index.js
@@ -1,19 +1,19 @@
-import mongoose from 'mongoose';
-import { DB_NAME } from '../constants.js';
-
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+let process_env_MONGODB_URI =
+	"mongodb+srv://piyushraikwar289:piyush289@cluster0.5lpcsrr.mongodb.net";
 const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(
-            `${process.env.MONGODB_URI}/${DB_NAME}`
-        );
-        console.log(`MongoDB Connected.. | DB Host : ${connectionInstance}`);
-        console.log(
-            `connectionInstance.connection.host : ${connectionInstance.connection.host}`
-        );
-    } catch (error) {
-        console.log(`MongoDB Connection Error : ${error}`);
-        process.exit(1);
-    }
+	try {
+		const connectionInstance = await mongoose.connect(
+			`${process_env_MONGODB_URI}/${DB_NAME}`
+		);
+		console.log(`MongoDB Connected.. | DB Host : ${connectionInstance}`);
+		console.log(
+			`connectionInstance.connection.host : ${connectionInstance.connection.host}`
+		);
+	} catch (error) {
+		console.log(`MongoDB Connection Error : ${error}`);
+		process.exit(1);
+	}
 };
-
 export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
-import connectDB from './db/index.js';
-dotenv.config({ path: './env' });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({ path: "./.env" });
 connectDB();
