9b11e42dbdbed02b31b5de2f8c1c40dc2dc6d7fe
Piyush289kumar <piyushraikwar289@gmail.com>
2024-04-10 10:56:37 +0530

    Create User Route

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asycHandler.utils.js";
 
 const registerUser = asyncHandler(async (req, res) => {
 	return res.status(200).json({
-		message: "ok",
+		message: "ok--",
 	});
 });
 
