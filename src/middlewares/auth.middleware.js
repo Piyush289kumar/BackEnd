02824c96c3409dca90c8c784bcde2197fcaa56d9
Piyush289kumar <piyushraikwar289@gmail.com>
2024-04-19 09:10:59 +0530

    Renew Token Tested

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,10 +7,11 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
 		const token =
 			req.cookies?.accessToken ||
 			req.header("Authorization")?.replace("Bearer ", "");
+
 		if (!token) {
 			throw new ApiError(401, "Unauthorized Request");
 		}
-		const decodedToken = jwt.verify(token, "1234");
+		const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 		const user = await User.findById(decodedToken?._id).select("-password");
 		if (!user) {
 			throw new ApiError(401, "Invalid Access Token");
